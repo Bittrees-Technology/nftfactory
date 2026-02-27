5cb779f75fd23a34cd9b576e8940c4c9f0ce8bc3
rmacfrensky <robertmacfrensky@proton.me>
2026-02-27 05:58:12 +0000

    fix(marketplace): add approval checks, reentrancy guard, and price validation

--- packages/contracts/src/interfaces/IERC1155Lite.sol
+++ packages/contracts/src/interfaces/IERC1155Lite.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.24;
 
 interface IERC1155Lite {
     function balanceOf(address account, uint256 id) external view returns (uint256);
+    function isApprovedForAll(address account, address operator) external view returns (bool);
     function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes calldata data) external;
 }
